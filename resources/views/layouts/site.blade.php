3894e59a30bf20af27689842660406e27d29fe04
paulo-web-dev <paulosergioorfanelli@gmail.com>
2026-05-27 12:02:40 -0300

    New tag manga

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-BR" class="dark">
 <head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18192995141"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18192995141');
+</script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#00A3FF">
@@ -51,13 +60,7 @@
       --bs-font-sans-serif: var(--font-body);
     }
   </style>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K7L7LBLS');</script>
-  <!-- End Google Tag Manager -->
+
   @stack('styles')
 </head>
 <body class="has-urgency-bar">
